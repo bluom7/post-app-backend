0758f83114ab350b599d0d9c2925034ba897b97d
bluomgroup-post <bluomgroup@gmail.com>
2026-06-28 15:55:10 +0530

    Initialize main.jsx with React and PostApp

---
+++ src/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PostApp from './PostApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PostApp />
+  </React.StrictMode>
+)
